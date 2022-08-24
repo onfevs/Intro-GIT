2ea81cf27a30f1684f89a4a84c1d3cbf04476f13
OnfeVS <jorgevallejo2990@gmail.com>
2022-08-24 09:42:42 -0500

    initial

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,6 @@
 class HolaMundo {
     public static void main(String[] args) {
-        String nombre;
-        saludar (nombre:"Jorge");
+        saludar("Jorge");
     }
 
     public static void saludar(String nombre) {
