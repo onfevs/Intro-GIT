2e7543f4cb0578b611ef907e45f03899b5bce9f0
OnfeVS <jorgevallejo2990@gmail.com>
2022-08-24 09:11:30 -0500

    update intro git/README.md and refactor intro-git/HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,10 @@
 class HolaMundo {
     public static void main(String[] args) {
+        String nombre;
+        Saludar (nombre:"Jorge")
+    }
 
+    public static void Saludar (String nombre){
+        System.out.println("Hola" + nombre + "!")
     }
 }
