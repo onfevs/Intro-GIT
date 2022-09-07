ffb3c628aca102d300dc3f4c678dfd759b8292d2
OnfeVS <jorgevallejo2990@gmail.com>
2022-09-07 10:00:30 -0500

    update app, conexion base de datos, creacion, eliminacion y actualizacion de mascotas

--- introduction-hibernate/README.md
+++ introduction-hibernate/README.md
@@ -1,7 +1,12 @@
-# Introduccion a Hibernate 
+# Introduction to Hibernate
 
-- Se instalan dependecias manuales MySQL e Hibernate del repositorio de MAVEN (https://mvnrepository.com/)
-- Se crea carpeta resources
-- Se crea archivo cfg.xml
-- Se crea archivo de configuracion, se coloca el mismo nombre de la base de datos, el usuario y la contrasena de la base de datos. Workbench.
-- Se crea dentro de introducion carpeta modelo y dentro Mascotas.java
+- new directory 'resources' and add file 'cfg.xml' into src/main
+
+### Maven repository
+
+- https://mvnrepository.com/
+
+### Dependencies
+
+- Mysql
+- Hibernate
--- introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/App.java
@@ -1,5 +1,74 @@
+package com.hibernate.introduction;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.introduction.modelo.Mascota;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Crear objeto que permita fabricar sesiones
+        SessionFactory factory = new Configuration()
+                .configure("cfg.xml")
+                .addAnnotatedClass(Mascota.class)
+                .buildSessionFactory();
+
+        // Abrir sessión
+        Session session = factory.openSession();
+
+        // Preparar la sesión para crear transacciones
+        session.beginTransaction();
+        try {
+            /**** Crear objetos *****/
+            // Mascota mascota_1 = new Mascota("Niño", "Quintero", "Criollo",
+            // "http://fake-photo", "Sobrepeso");
+            // Mascota mascota_2 = new Mascota("Dulce", "Medina", "Bulldog",
+            // "http://fake-photo", "Ninguna");
+            // Mascota mascota_3 = new Mascota("Firulais", "Jimenez", "Criollo",
+            // "http://fake-photo", "Agresivo");
+            // Mascota mascota_4 = new Mascota("Brandom", "Tercer", "Beagle",
+            // "http://fake-photo", "Jugueton");
+
+            // Guardar los objetos
+
+            // session.persist(mascota_4);
+            // // Enviar datos a la BD
+            // session.getTransaction().commit();
+
+            /***** Buscar entidad/registro/mascota ********/
+
+            // Mascota mascota = session.byId(Mascota.class).load(6);
+            // System.out.println(mascota);
+            // // Eliminar
+            // session.remove(mascota);
+            // session.getTransaction().commit();
+
+            // Mascota mascota = session.find(Mascota.class, 4);
+            // System.out.println(mascota);
+
+            // // Actualizar
+            // mascota.setNombre("Brandom");
+            // mascota.setApellido("Tercero");
+            // mascota.setRaza("Beagle Ingles");
+            // mascota.setObservacion("Jugueton");
+
+            // session.merge(mascota);
+            // session.getTransaction().commit();
+
+            // List<Mascota> mascotas = session.createQuery("from mascotas",
+            // Mascota.class).list();
+
+            // for (int i = 0; i < mascotas.size(); i++) {
+            // System.out.println(mascotas.get(i));
+            // }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Cerrar sesión
+        session.close();
     }
 }
